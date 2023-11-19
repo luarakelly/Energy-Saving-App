c8ebcfbf308b6423ddbb7eb19770b011e877ed80
luarakelly <lua.kellly@gmail.com>
2023-11-19 22:21:34 +0000

    django dowload: python -m pip install Django
    setting the django project: django-admin startproject web_project .

--- main.py
+++ main.py
@@ -6,42 +6,44 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
 valmet_he = yf.Ticker("VALMT.HE")
-#print(valmet_he.calendar)
-#print(valmet_he.institutional_holders)
-#print(valmet_he.recommendations)
+# print(valmet_he.calendar)
+# print(valmet_he.institutional_holders)
+# print(valmet_he.recommendations)
 # get all stock info
-#valmet_he.info
+# valmet_he.info
 
 # get historical market data
-#hist = valmet_he.history(period="1mo")
-#save data
+# hist = valmet_he.history(period="1mo")
+# save data
 stock_symbol = "VALMT.HE"
 start_date = "2013-01-01"
 end_date = "2023-11-03"
 # Fetch the daily historical data
-#data = yf.download(stock_symbol, start=start_date, end=end_date)
-#data.to_csv('daily_valmetHE_stock_data.csv')
-#--------------------------------------------------------------------------------------------------------
+# data = yf.download(stock_symbol, start=start_date, end=end_date)
+# data.to_csv('daily_valmetHE_stock_data.csv')
+# --------------------------------------------------------------------------------------------------------
 
 # Load your data into a DataFrame
-data = pd.read_csv('your_data.csv')
+data = pd.read_csv("your_data.csv")
 
 # Data Preprocessing
-data['Date'] = pd.to_datetime(data['Date'])
-data.set_index('Date', inplace=True)
+data["Date"] = pd.to_datetime(data["Date"])
+data.set_index("Date", inplace=True)
 
 # Create a target variable by shifting the open and close prices by one day
-data['Next_Open'] = data['Open'].shift(-1)
-data['Next_Close'] = data['Close'].shift(-1)
+data["Next_Open"] = data["Open"].shift(-1)
+data["Next_Close"] = data["Close"].shift(-1)
 
 # Feature Engineering (if desired)
 # You can add additional features here
 
 # Data Splitting
-X = data[['Open', 'Close']].iloc[:-1]  # Features
-y = data[['Next_Open', 'Next_Close']].iloc[:-1]  # Target
+X = data[["Open", "Close"]].iloc[:-1]  # Features
+y = data[["Next_Open", "Next_Close"]].iloc[:-1]  # Target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Random Forest Model
 rf_model = RandomForestRegressor(n_estimators=100, max_depth=10, random_state=42)
