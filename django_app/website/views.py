0c359d9e01a34ceb5b075c54d4f0c6c24404f0d1
luarakelly <lua.kellly@gmail.com>
2023-11-21 19:11:55 +0000

    succeful login

--- django_app/website/views.py
+++ django_app/website/views.py
@@ -7,17 +7,17 @@ def home(request):
     # Check to see if loged in
     if request.method == 'POST': #after filling the form, whn you click submit, it will send a http request to the server, with the method in it.
         # to get the information tiped by the user:
-        user_name = request.POST['user_name']
+        username = request.POST['username']
         password = request.POST['password']
         # to Authenticate:
         #in this case we used the user input as authentication, but instead of it, it should point to the informations saved in the database user_profile.
-        auth = authenticate(request, user_name= user_name, password= password)
-        if auth is not None:
-            login(request, auth)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
             messages.success(request, 'You are logged in!')
             return redirect('home')
         else:
-            messages.success(request, 'Wrong user name or password, recheck it and try again.')
+            messages.success(request, 'Wrong user name or password. Please try again.')
             return redirect('home')
     else:  
         return render(request, 'home.html', {})
