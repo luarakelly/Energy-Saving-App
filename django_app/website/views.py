c350f160a68eef58162aa35d53a165a0a5f74a50
luarakelly <lua.kellly@gmail.com>
2023-11-21 23:11:28 +0000

    register page - nedds fixing.

--- django_app/website/views.py
+++ django_app/website/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
 def home(request):
     # Check to see if loged in
     if request.method == 'POST': #after filling the form, whn you click submit, it will send a http request to the server, with the method in it.
@@ -33,4 +33,19 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #clean the form:
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #Authenticate and log in:
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You registered successfully. Welcome!')
+            return redirect('home')
+        else:
+            form = SignUpForm()
+            return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
